76d4adaa3c748ca85762ed97cc33a704df833590
jjenk <j.jenkinson89@hotmail.com>
2026-08-23 17:15:19 +0200

    Init project - basic file structure setup - create Pages and Components folders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
+import { Merch } from './Pages/merch/merch';
+import { Home } from './Pages/home/home';
+import { About } from './Pages/about/about';
+import { Tour } from './Pages/tour/tour';
+import { Gallery } from './Pages/gallery/gallery';
+import { Contact } from './Pages/contact/contact';
+import { NotFound } from './Pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+        title: 'Home',
+    },
+    {
+        path: 'about',
+        component: About,
+        title: 'About',
+    },
+    {
+        path: 'tour',
+        component: Tour,
+        title: 'Tour Dates',
+    },
+    {
+        path: 'merch',
+        component: Merch,
+        title: 'Merch',
+    },
+    {
+        path: 'gallery',
+        component: Gallery,
+        title: 'Gallery',
+    },
+    {
+        path: 'contact',
+        component: Contact,
+        title: 'Contact',
+    },
+    {
+        path: '**',
+        component: NotFound,
+        title: '404 Not Found',
+    }
+];
